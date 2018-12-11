ff1d9a3c946ff271395f7cab3a8b6fc214641bba
kostya <zik1554@bk.ru>
2018-12-11 22:55:56 +0300

    first commit

---
+++ src/main/java/hello/api/users/web/UsersController.java
@@ -0,0 +1,151 @@
+package hello.api.users.web;
+
+import hello.api.users.model.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import hello.api.users.service.UserService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api-users")
+public class UsersController {
+    private static final Logger logger = LoggerFactory.getLogger(UsersController.class);
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/get{uuid}")
+    public ResponseEntity<UserInfo> getUser(@RequestParam UUID uuid) {
+        try {
+
+
+            return new ResponseEntity(userService.findUserByUid(uuid), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("getError", e);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UUID> createUser(@RequestBody UserInfo requestUserDetails) {
+        try {
+
+
+            UUID uuid= userService.registrationUser(requestUserDetails);
+            if (uuid != null) {
+                return new ResponseEntity(uuid,HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+            }
+
+        } catch (Exception e) {
+            logger.error("createError", e);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete{uuid}")
+    public ResponseEntity deleteUser(@RequestParam UUID uuid) {
+
+        try {
+
+
+            userService.deleteUser(uuid);
+
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("deleteError", e);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity updateUser(@RequestBody UserInfo requestUserDetails) {
+        try {
+
+
+            userService.updateUser(requestUserDetails);
+
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("updateUserError", e);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/updateUUID")
+    public ResponseEntity updateUuidUser(@RequestBody UserInfo requestUserDetails) {
+        try {
+
+
+           UUID newUuid=userService.updateUuidUser(requestUserDetails.getUid());
+
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("updateUuidUserError", e);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/updateVK")
+    public ResponseEntity updateVKUser(@RequestBody UserInfo requestUserDetails) {
+        try {
+
+
+            userService.updateVkUser(requestUserDetails);
+
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("updatevkUserError", e);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping("/login")
+    public ResponseEntity<UUID> loginUser(@RequestBody UserInfo requestUserDetails) {
+        try {
+
+            if (userService.loginUser(requestUserDetails).equals("Success")) {
+                return new ResponseEntity(HttpStatus.OK);
+            } else {
+                return new ResponseEntity(HttpStatus.LOCKED);
+            }
+
+        } catch (Exception e) {
+            logger.error("loginError", e);
+            return new ResponseEntity(HttpStatus.LOCKED);
+           //
+            // return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<UserInfo>> findAllUsers() {
+        try {
+
+
+            List<UserInfo> users = userService.findAllUsers();
+
+            if (users.isEmpty()) {
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+
+            } else {
+                return new ResponseEntity<List<UserInfo>>(users, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            logger.error("getAllError", e);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+}
